788a29361142b16821745b1eef8472e6a7173bc9
Jarryd <jarrydleroux94@gmail.com>
2026-05-19 00:07:17 +0200

    update

--- src/server.ts
+++ src/server.ts
@@ -25,7 +25,7 @@ const angularApp = new AngularNodeAppEngine();
  */
 
 /**
- * Serve static files from /browser
+ * Serve static files from /browser 
  */
 app.use(
   express.static(browserDistFolder, {
